c1400f8d96b9d301831287492907ab28e6c1413d
Maxim <Crus_nic@hotmail.com>
2021-11-17 19:13:42 +0300

    изменил OptionsReducer

--- src/components/options/OptionsReducer.tsx
+++ src/components/options/OptionsReducer.tsx
@@ -1,23 +1,77 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+    PutOptionsToServer,
+    IOption,
+    GetOptionsFromServer,
+} from '../../services/ServerMock';
+import { SpeedEnum } from './speedRegulator/SpeedRegulatorReducer';
+
+export interface IOptionState {
+    rows: number;
+    cols: number;
+    percent: number;
+    speed: number;
+    status: OptionsLoadingEnum;
+}
+
+export enum OptionsLoadingEnum {
+    Loading,
+    Complete,
+    Error,
+}
+
+export const getOptionsFromServer = createAsyncThunk(
+    'options/GetOptionsFromServer',
+    async (userName: string) => {
+        const response = (await GetOptionsFromServer(userName)) as IOption;
+        return response;
+    },
+);
 
 export const optionSlice = createSlice({
     name: 'login',
     initialState: {
-        isLoading: false,
         rows: 0,
         cols: 0,
-        percent: 0
-    },
+        percent: 0,
+        speed: SpeedEnum.Slow,
+        status: OptionsLoadingEnum.Loading,
+    } as IOptionState,
     reducers: {
-        loadOptions: (state, action) => {
-            
+        applyOptions: (state, { payload }: PayloadAction<string>) => {
+            PutOptionsToServer(payload, {
+                rows: state.rows,
+                cols: state.cols,
+                percent: state.percent,
+                speed: state.speed,
+            } as IOption);
         },
-        applyOptions: state => {
-           
+        changeOptions: (state, { payload }) => {
+            state.cols = payload.cols;
+            state.rows = payload.rows;
+            state.percent = payload.percent;
+            state.speed = payload.speed;
         },
+    },
+    extraReducers: builder => {
+        builder.addCase(getOptionsFromServer.pending, state => {
+            state.status = OptionsLoadingEnum.Loading;
+        });
+        builder.addCase(
+            getOptionsFromServer.fulfilled,
+            (state, { payload }) => {
+                state.status = OptionsLoadingEnum.Complete;
+                state.cols = payload.cols;
+                state.rows = payload.rows;
+                state.percent = payload.percent;
+                state.speed = payload.speed;
+            },
+        );
+        builder.addCase(getOptionsFromServer.rejected, state => {
+            state.status = OptionsLoadingEnum.Error;
+        });
     },
 });
 
-export const { loadOptions, applyOptions } = optionSlice.actions;
-
+export const { applyOptions, changeOptions } = optionSlice.actions;
 export default optionSlice.reducer;
